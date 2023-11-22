7dcc282f62e4d84d53776467d79392e4b63bbdcc
rbethke <randydbethke@gmail.com>
2023-11-22 00:51:24 -0600

    quick edits

---
+++ buffalo-bulldogs-react/src/components/hero/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './HeroStyles.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='container'>
+                <div className='content'>
+                    <h1>Local</h1>
+                    <h1 className='maroon'>Family Friendly</h1>
+                    <h1>Entertainment</h1>
+                    <div><button>Schedule</button></div>
+                    </div>    
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Hero
